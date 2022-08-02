6fc0eb8fb43ae876632d8ea6fa11099791ac9606
Brad <bradleyrosenlof@gmail.com>
2022-08-02 07:08:41 -0500

    lambda tf

--- _variables.tf
+++ _variables.tf
@@ -1,17 +1,28 @@
 variable "kms_key_arn" {
+  type = string
 }
 
 variable "secret_arn" {
+  type = string
 }
 
 variable "acm_certificate_arn" {
+  type = string
 }
 
 variable "keyname_certificate" {
+  type = string
 }
 
 variable "keyname_private_key" {
+  type = string
 }
 
 variable "keyname_certificate_chain" {
+  type = string
+}
+
+variable "cloudwatch_log_retention_days" {
+  type = number
+  default = 90
 }
---
+++ lambda.tf
@@ -0,0 +1,117 @@
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+module "lambda" {
+  source  = "app.terraform.io/SevenPico/lambda-function/aws"
+  version = "0.1.0.2"
+  context = module.this.context
+
+  architectures                       = null
+  cloudwatch_event_rules              = {}
+  cloudwatch_lambda_insights_enabled  = false
+  cloudwatch_logs_kms_key_arn         = null
+  cloudwatch_logs_retention_in_days   = var.cloudwatch_log_retention_days
+  cloudwatch_log_subscription_filters = {}
+  description                         = "Update ACM, ECS, and EC2 with SSl certificate from secret."
+  event_source_mappings               = {}
+  filename                            = one(data.archive_file.lambda[*].output_path)
+  function_name                       = module.this.id
+  handler                             = "main.lambda_handler"
+  ignore_external_function_updates    = false
+  image_config                        = {}
+  image_uri                           = null
+  kms_key_arn                         = ""
+  lambda_at_edge                      = false
+  layers                              = []
+  memory_size                         = 128
+  package_type                        = "Zip"
+  publish                             = false
+  reserved_concurrent_executions      = -1
+  role_name                           = "${module.this.id}-role"
+  runtime                             = "python3.9"
+  s3_bucket                           = null
+  s3_key                              = null
+  s3_object_version                   = null
+  sns_subscriptions                   = {}
+  source_code_hash                    = one(data.archive_file.lambda[*].output_sha)
+  ssm_parameter_names                 = null
+  timeout                             = 3
+  tracing_config_mode                 = null
+  vpc_config                          = null
+
+  lambda_environment = {
+    variables = {
+      SECRET_ARN : var.secret_arn
+    }
+  }
+}
+
+data "archive_file" "lambda" {
+  count       = module.this.enabled ? 1 : 0
+  type        = "zip"
+  source_dir  = "${path.module}/lambda"
+  output_path = "${path.module}/.build/lambda.zip"
+}
+
+
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# resource "aws_lambda_permission" "FIXME" {
+#   count         = module.this.enabled ? 1 : 0
+#   statement_id  = "FIXME"
+#   action        = "lambda:InvokeFunction"
+#   function_name = module.lambda.function_name
+#   principal     = FIXME
+# }
+
+
+# ------------------------------------------------------------------------------
+# Lambda IAM
+# ------------------------------------------------------------------------------
+data "aws_iam_policy_document" "lambda" {
+  count = module.this.enabled ? 1 : 0
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "secretsmanager:GetSecretValue",
+      "secretsmanager:DescribeSecret",
+    ]
+    resources = [
+      var.secret_arn
+    ]
+  }
+  statement {
+    effect = "Allow"
+    actions = [
+      "kms:Decrypt",
+      "kms:DescribeKey",
+    ]
+    resources = [
+      var.kms_key_arn
+    ]
+  }
+  statement {
+    effect = "Allow"
+    actions = [
+      "ssm:SendCommand",
+    ]
+    resources = ["*"]
+    # condition {
+    #   test = "StringLike"
+    #   variable = "ssm:resourceTag/Name"
+    #   values = [FIXME]
+    # }
+  }
+}
+
+resource "aws_iam_policy" "lambda" {
+  count  = module.this.enabled ? 1 : 0
+  name   = "${module.this.id}-lambda"
+  policy = one(data.aws_iam_policy_document.lambda[*].json)
+}
+
+resource "aws_iam_role_policy_attachment" "lambda" {
+  count      = module.this.enabled ? 1 : 0
+  role       = module.lambda.role_arn #"${module.this.id}-role"
+  policy_arn = one(aws_iam_policy.lambda[*].arn)
+}
