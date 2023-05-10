c8bd9747b4db89fac8eb037d1e0b3512f93e1ef8
Adarsh Hiwrale <ahiwrale@sigmasolve.net>
2023-05-10 20:29:43 +0530

    update secret read principles

--- examples/ssl-updater/ssl.tf
+++ examples/ssl-updater/ssl.tf
@@ -51,14 +51,18 @@ module "ssl_certificate" {
   keyname_private_key               = "CERTIFICATE_PRIVATE_KEY"
   kms_key_deletion_window_in_days   = 10
   kms_key_enable_key_rotation       = false
-  secret_read_principals            = {}
-  secret_update_sns_pub_principals  = {
-    Event = {
+  secret_read_principals            = {
+    Events = {
       type        = "Service"
-      identifiers = ["events.amazonaws.com"]
-      condition   = null
+      identifiers = ["event.amazonaws.com"]
+      condition   = {
+        test     = null
+        values   = []
+        variable = ""
+      }
     }
   }
+  secret_update_sns_pub_principals  = {}
   secret_update_sns_sub_principals  = {}
   zone_id                           = null
 }
