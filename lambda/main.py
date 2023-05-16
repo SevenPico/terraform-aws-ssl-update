3269ec38643fd1bba378aca062bd77115e2ce0a7
Adarsh Hiwrale <ahiwrale@sigmasolve.net>
2023-05-16 23:09:10 +0530

    FIX me comment

--- lambda/main.py
+++ lambda/main.py
@@ -166,5 +166,5 @@ def ecs_service_update():
     # ECS.Client.exceptions.ClusterNotFoundException
     # ECS.Client.exceptions.ServiceNotFoundException
 
-
+# FIXME After commenting out lambda_handler(None, None), the ssm_ssl_named_document function does not execute twice..
 # lambda_handler(None, None)
