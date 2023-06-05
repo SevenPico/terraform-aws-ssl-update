865f99b9c48f0fd4e70b06dfd61e4e962e879c49
mani <mpanneerselvam@sigmasolve.net>
2023-06-05 23:05:55 +0530

    fmt

--- examples/complete/openvpn.tf
+++ examples/complete/openvpn.tf
@@ -111,7 +111,7 @@ module "openvpn" {
   ec2_role_source_policy_documents          = try(data.aws_iam_policy_document.openvpn_ec2_policy_doc[*].json, [])
   ec2_upgrade_schedule_expression           = "cron(15 13 ? * SUN *)"
   ec2_security_group_allow_all_egress       = true
-  ec2_security_group_rules = []
+  ec2_security_group_rules                  = []
 
   # NLB
   nlb_access_logs_prefix_override = null
