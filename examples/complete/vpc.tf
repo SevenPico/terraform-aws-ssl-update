b99dc256bb7283ff3cbf4450890e39b257627abd
mani <mpanneerselvam@sigmasolve.net>
2023-06-08 18:45:00 +0530

    reverting changes in vpc to check

--- examples/complete/vpc.tf
+++ examples/complete/vpc.tf
@@ -39,19 +39,27 @@ module "vpc_subnets_context" {
 #------------------------------------------------------------------------------
 module "vpc" {
   source  = "registry.terraform.io/cloudposse/vpc/aws"
-  version = "2.0.0"
+  version = "0.28.1"
   context = module.vpc_context.legacy
 
-  assign_generated_ipv6_cidr_block          = false
-  default_network_acl_deny_all              = false
-  default_route_table_no_routes             = false
-  default_security_group_deny_all           = true
-  dns_hostnames_enabled                     = true
-  dns_support_enabled                       = true
-  instance_tenancy                          = "default"
-  internet_gateway_enabled                  = true
-  ipv4_primary_cidr_block                   = var.vpc_cidr_block
-  ipv6_egress_only_internet_gateway_enabled = false
+  cidr_block                                      = var.vpc_cidr_block
+  additional_cidr_blocks                          = []
+  assign_generated_ipv6_cidr_block                = false
+  classiclink_dns_support_enabled                 = false
+  classiclink_enabled                             = false
+  default_security_group_deny_all                 = true
+  dns_hostnames_enabled                           = true
+  dns_support_enabled                             = true
+  enable_classiclink                              = false
+  enable_default_security_group_with_custom_rules = false
+  enable_classiclink_dns_support                  = false
+  enable_dns_hostnames                            = true
+  enable_dns_support                              = true
+  enable_internet_gateway                         = true
+  instance_tenancy                                = "default"
+  internet_gateway_enabled                        = true
+  ipv6_egress_only_internet_gateway_enabled       = false
+  ipv6_enabled                                    = true
 }
 
 
@@ -59,8 +67,8 @@ module "vpc" {
 # VPC Subnets
 #------------------------------------------------------------------------------
 module "vpc_subnets" {
-  source  = "registry.terraform.io/cloudposse/dynamic-subnets/aws"
-  version = "1.0.0"
+  source  = "cloudposse/dynamic-subnets/aws"
+  version = "0.39.8"
   context = module.vpc_subnets_context.legacy
 
   availability_zones                   = var.availability_zones
@@ -77,7 +85,6 @@ module "vpc_subnets" {
   metadata_http_tokens_required        = true
   nat_elastic_ips                      = []
   nat_gateway_enabled                  = true
-  nat_instance_cpu_credits_override    = ""
   nat_instance_enabled                 = false
   nat_instance_type                    = "t3.micro"
   private_network_acl_id               = ""
